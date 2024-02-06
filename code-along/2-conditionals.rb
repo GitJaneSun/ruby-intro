acf3562412a7b10b82b8d0a4e8ab5741ad33d2b2
Jane Sun <junwen.sun@kellogg.northwestern.edu>
2024-02-06 04:58:39 +0000

    conditionals-1

--- code-along/2-conditionals.rb
+++ code-along/2-conditionals.rb
@@ -3,6 +3,13 @@
 # ruby 2-conditionals.rb
 
 # Booleans
+# if 1 + 1 == 2
+#     puts "this is true"
+# end
+
+# if 1 + 1 == 3
+#     puts "you should never see this"
+# end
 
 # Boolean Expressions
 
@@ -13,3 +20,37 @@
 # Elsif Conditional Logic
 
 # Combining Expressions
+
+# input
+# user_entered_password = "k"
+# real_password = "tacos"
+
+# # algorithm
+# if user_entered_password == real_password
+#     # output
+#     puts "you are in!"
+# else
+#     puts "sorry, wrong passwords"
+# end
+
+# my_team_score = 5
+# other_team_score = 5
+# if my_team_score > other_team_score
+#     puts "we win!"
+# elsif my_team_score = other_team_score
+#     puts "we tied!"
+# else
+#     puts "we lost!"
+# end
+
+# combining expressions
+temperature = 31
+precip = 100
+
+if temperature > 60 && precip == 0
+    puts "It's nice outside!"
+elsif temperature <= 60 && precip == 0
+    puts "It's cold but sunny!"
+else 
+    puts "It's gross!" 
+end
